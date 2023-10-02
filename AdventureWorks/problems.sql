7340a0c2625d5500d7ffb480764d28a6c19bec8d
Mehrshad <lastenigmas@gmail.com>
2023-10-02 20:01:49 +0330

    get that

--- AdventureWorks/problems.sql
+++ AdventureWorks/problems.sql
@@ -3,3 +3,8 @@ select pass.PasswordSalt , person.FirstName from Person.Password as pass
 inner join Person.Person as person
 on pass.BusinessEntityID = person.BusinessEntityID
 
+--2 -> 100 From the following table write a query in SQL to calculate the sum
+-- of the ListPrice and StandardCost for each color. Return color, sum of ListPrice.
+select color , avg(listPrice) as price , avg(StandardCost) as cost
+from Production.Product
+group by color
